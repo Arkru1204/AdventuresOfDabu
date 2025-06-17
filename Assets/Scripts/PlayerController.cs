bd6ad135d1aa24242dd7e1feb2d7bb70222b4719
Arkru <35932023+Arkru1204@users.noreply.github.com>
2025-06-18 00:43:45 +0900

    [Feat] Move Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rigid;
+
+    private InputAction moveAction;
+
+    private Vector2 moveInput;
+
+    [Header("Move")]
+    [SerializeField, Range(0f, 10f)]
+    private float moveSpeed = 10;
+
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        GetInput();
     }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetInput()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        if (moveInput.x != 0)
+        {
+            rigid.linearVelocity = new Vector2(moveInput.x * moveSpeed, rigid.linearVelocity.y);
+        }
+        else
+        {
+            rigid.linearVelocity = new Vector2(0f, rigid.linearVelocity.y);
+        }
+    }
+
 }
